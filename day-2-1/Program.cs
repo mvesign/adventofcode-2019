40676a793fc2aae6d9f52ece21fabd6fd5d79303
Maikel Vreugde <git@mvesign.com>
2020-01-03 22:41:01 +0100

    First part of day 2

---
+++ day-2-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void ProcessIntcode(int[] intcode)
+            {
+                for(var position = 0; position + 4 < intcode.Length; position += 4)
+                {
+                    var noun = intcode[position + 1];
+                    var verb = intcode[position + 2];
+                    var result = intcode[position + 3];
+
+                    switch (intcode[position])
+                    {
+                        case 1:
+                            intcode[result] = intcode[noun] + intcode[verb];
+                            break;
+                        case 2:
+                            intcode[result] = intcode[noun] * intcode[verb];
+                            break;
+                        case 99:
+                            return;
+                    }
+                }
+            }
+
+            var intcode = File.ReadAllText("input.txt")
+                .Split(',')
+                .Select(value => int.Parse(value))
+                .ToArray();
+            
+            intcode[1] = 12;
+            intcode[2] = 2;
+
+            ProcessIntcode(intcode);
+            
+            Console.WriteLine($"Value: {intcode[0]}");
+        }
+    }
+}
